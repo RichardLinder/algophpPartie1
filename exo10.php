992a4974f6c9a40ff9e82c5cee2bb6c0e4401a33
RichardLinder <55447316+RichardLinder@users.noreply.github.com>
2023-03-21 16:53:59 +0100

    fin de cour

--- exo10.php
+++ exo10.php
@@ -1,4 +1,4 @@
-<h1> Exercice  </h1>
+<h1> Exercice 10 </h1>
 
 <!-- énoncé -->
 <P>A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui<br>
@@ -10,14 +10,28 @@
 $dix=0;
 $cinq=0;
 $deux=0;
-$un=0;
-$prix=;
-$versé;
+$prix=152;
+$versé=200;
+$rendu= $versé-$prix;
+
+division entière*******************************************************************************************************************************************************************
+
+$reste= $rendu;
 
 
+
+$dix== $reste % 10;
+
+$reste== $rendu-(10*$dix);
+
+$cinq==$reste%5;
+$reste== $rendu-(5-$cinq);
+
+$deux==$reste%2;
+$reste== $rendu-(2*$deux);
 
 echo "Montant à payer : $prix €<br>;";
 echo "Montant versé : $versé €<br>;";
-echo "rendu  :$rendu  €<br>;";
+echo "à rendre  :$rendu  €<br>;";
 echo "Rendue de monnaie :<br>;";
-echo "$dix billets de 10 € $cinq billet de 5 € $deux pièce de 2 € $un pièce de 1 €";
+echo "$dix billets de 10 € $cinq billet de 5 € $deux pièce de 2 € $reste pièce de 1 €";
